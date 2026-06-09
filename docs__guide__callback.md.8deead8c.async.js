914b53aed34315928681f0a738c7133719c75022
mfuu <mfuu.me@gmail.com>
2026-06-09 01:30:08 +0000

    Deploying to gh-pages from @ mfuu/react-virtual-sortable@64a762fe333849c83e2a3c8ef4c628a15e10dad1 🚀

---
+++ docs__guide__callback.md.8deead8c.async.js
@@ -0,0 +1,23 @@
+"use strict";(self.webpackChunkreact_virtual_sortable=self.webpackChunkreact_virtual_sortable||[]).push([[238],{82469:function(l,n,t){t.r(n);var r=t(72269),u=t(93359),c=t(61788),x=t(19977),v=t(95350),a=t(24268),h=t(96057),m=t(83213),d=t(75576),o=t(89188),s=t(67294),_=t(4991),e=t(85893);function i(){return(0,e.jsx)(d.dY,{children:(0,e.jsx)(s.Suspense,{fallback:(0,e.jsx)(o.Z,{}),children:(0,e.jsx)(e.Fragment,{children:(0,e.jsxs)("div",{className:"markdown",children:[(0,e.jsxs)("h2",{id:"ontop",children:[(0,e.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#ontop",children:(0,e.jsx)("span",{className:"icon icon-link"})}),(0,e.jsx)("code",{children:_.texts[0].value})]}),(0,e.jsx)("p",{children:_.texts[1].value}),(0,e.jsxs)("h2",{id:"onbottom",children:[(0,e.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#onbottom",children:(0,e.jsx)("span",{className:"icon icon-link"})}),(0,e.jsx)("code",{children:_.texts[2].value})]}),(0,e.jsx)("p",{children:_.texts[3].value}),(0,e.jsxs)("h2",{id:"onscroll",children:[(0,e.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#onscroll",children:(0,e.jsx)("span",{className:"icon icon-link"})}),(0,e.jsx)("code",{children:_.texts[4].value})]}),(0,e.jsx)("p",{children:_.texts[5].value}),(0,e.jsx)(a.Z,{lang:"ts",children:_.texts[6].value}),(0,e.jsxs)("h2",{id:"ondrag",children:[(0,e.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#ondrag",children:(0,e.jsx)("span",{className:"icon icon-link"})}),(0,e.jsx)("code",{children:_.texts[7].value})]}),(0,e.jsx)("p",{children:_.texts[8].value}),(0,e.jsx)(a.Z,{lang:"ts",children:_.texts[9].value}),(0,e.jsxs)("h2",{id:"ondrop",children:[(0,e.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#ondrop",children:(0,e.jsx)("span",{className:"icon icon-link"})}),(0,e.jsx)("code",{children:_.texts[10].value})]}),(0,e.jsx)("p",{children:_.texts[11].value}),(0,e.jsx)(a.Z,{lang:"ts",children:_.texts[12].value})]})})})})}n.default=i},4991:function(l,n,t){t.r(n),t.d(n,{texts:function(){return r}});const r=[{value:"onTop",paraId:0},{value:"scrolled to the top of list",paraId:1,tocIndex:0},{value:"onBottom",paraId:0},{value:"scrolled to the bottom of list",paraId:2,tocIndex:1},{value:"onScroll",paraId:0},{value:"scroll event",paraId:3,tocIndex:2},{value:`const {
+  top,
+  bottom,
+  offset,
+  direction,
+} = scrollEvent
+`,paraId:4,tocIndex:2},{value:"onDrag",paraId:0},{value:"drag is started",paraId:5,tocIndex:3},{value:`const {
+  item,
+  key,
+  index,
+  event,
+} = dragEvent
+`,paraId:6,tocIndex:3},{value:"onDrop",paraId:0},{value:"drag is completed",paraId:7,tocIndex:4},{value:`const {
+  key,
+  item,
+  list,
+  event,
+  changed,
+  oldList,
+  oldIndex,
+  newIndex,
+} = dropEvent
+`,paraId:8,tocIndex:4}]}}]);
